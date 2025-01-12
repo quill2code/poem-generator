9932070d510e96aeb8b5798d06061d0eabb12424
quill2code <denisehjohnson@yahoo.com>
2025-01-12 14:24:29 -0700

    Update index.js
    add js code

---
+++ AI poem generator/index.js
@@ -0,0 +1,4 @@
+let poemFormElement = document.querySelector ('#poem-function generate Poem(event)
+                                              event preventDefault();
+alert ("generating poem");
+poemFormElement.addEventListener("submit", generatePoem);
