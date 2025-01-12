6971ef84a8a8d6caf6cc4fd4e8a670efa0a2defe
quill2code <denisehjohnson@yahoo.com>
2025-01-12 15:11:54 -0700

    Update index.js
    corrected js

--- AI poem generator/index.js
+++ AI poem generator/index.js
@@ -1,10 +1,13 @@
-let poemFormElement = document.querySelector ("#poem-function generate Poem(event) {
-                                          
-                                              event preventDefault();
-alert ("generating poem");
-letpoemFormElement = document.querySelector ("#poem-generator-form");
-let PoemElement = documentquerySelector ("#poem");
-poemElement.innerHTML = "Roses are red";
-poemFormElement.addEventListener("submit", generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
